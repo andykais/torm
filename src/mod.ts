092d88622b5c36d42cff77f611631feeb83fa05d
Andrew Kaiser <kaisea.rpi@gmail.com>
2023-01-19 10:39:55 -0500

    schema, query works. Supports array params

--- src/mod.ts
+++ src/mod.ts
@@ -1,25 +1,105 @@
-import type { Database as SqliteNative } from '../../sqlite-native/src/database.ts'
-import { Model } from './model.ts'
+// type SchemaGeneric = {
+//     [field: string]: ZodSchema<any, any>
+// }
+// type BuildSchemaFieldGeneric = {
+//     encode: ZodSchema<any, any>
+//     decode: ZodSchema<any, any>
+//     field_name: string
+// }
+// type BuildSchemaGeneric = {
+//     [field: string]: BuildSchemaFieldGeneric
+// }
+// type BuildSchemaField<Name extends string, Encode extends ZodSchema<any, any>, Decode extends ZodSchema<any, any>> = {
+//     encode: Encode
+//     decode: Decode
+//     field_name: Name
+// }
+// type BuildSchema<T extends SchemaGeneric> = {
+//     [K in keyof T]: {
+//         encode: T[K]
+//         decode: T[K]
+//         field_name: K
+//     }
+// }
 
-type Driver =
-  | SqliteNative
 
-type ModelClass<S, Q> = typeof Model<S, Q>
-type InstantiatedModel<M extends ModelClass<S, Q>, S = any, Q = any> = Model<S, Q>
+// function schema<T extends SchemaGeneric>(schema: T) {
+//     const built_schema: Partial<BuildSchema<T>> = {}
+//     Object.keys(schema).forEach((field: keyof T) => {
+//         built_schema[field] = {
+//             encode: schema[field],
+//             decode: schema[field],
+//             field_name: field
+//         }
+//     })
+//     return built_schema as BuildSchema<T>
+// }
 
-class Torm<M extends Record<string, ModelClass<any, any>>> {
-  models: { [K in keyof M]: InstantiatedModel<M[K]> }
-  public constructor(private driver: Driver, models: M) {
-    const instantiated_models: Record<string, Model<any, any>> = {}
-    for (const model_name of Object.keys(models)) {
-      instantiated_models[model_name] = models[model_name]
-    }
-    this.models = instantiated_models as any
-  }
-}
+// type AllKeys<T> = T extends any ? keyof T : never;
+// type PickType<T, K extends AllKeys<T>> = T extends { [k in K]?: any }
+//     ? T[K]
+//     : never;
+// type Merge<T extends object> = {
+//   [k in AllKeys<T>]: PickType<T, k>;
+// }
 
-function torm<M extends Record<string, ModelClass<any, any>>>(driver: Driver, models: M) {
-  return new Torm(driver, models)
-}
+// type StatementParams<T extends BuildSchemaFieldGeneric[]> =
+//     Merge<T extends Array<infer B>
+//         ? B extends BuildSchemaField<infer Name, any, any>
+//             ? { [K in Name]: ZodInput<B['encode']> }
+//             : never
+//         : never>
 
-export { torm }
+
+// function query<T extends BuildSchemaFieldGeneric[]>(strings: TemplateStringsArray, ...params: T): Statement<StatementParams<T>, {}> {
+//     return {
+//         one:  (params: {}) => ({}),
+//         all:  (params: {}) => [],
+//         exec: (params: {}) => {},
+//         params: {} as StatementParams<T>
+//     }
+// }
+
+// interface Statement<Params extends {}, Result> {
+//     one: (params: Params) => Result
+//     all: (params: Params) => Result[]
+//     exec: (params: Params) => void
+//     params: Params
+// }
+
+// type ZodSchema<In, Out> = (param: In) => Out
+// const z = {
+//     string: () => ({}) as ZodSchema<string, string>,
+//     number: () => ({}) as ZodSchema<number, number>,
+// }
+// type ZodInput<T extends ZodSchema<any, any>> = T extends ZodSchema<infer In, any>
+//     ? In
+//     : never
+
+// // usage
+// class Book {
+//     static schema = schema({
+//         isbn: z.string(),
+//         author_id: z.number()
+//     })
+
+
+//     get = query`SELECT ${Book.schema.author_id} FROM book WHERE isbn = ${Book.schema.isbn}`
+
+//     // constructor(driver: SQLite) {
+//     //     this.get = driver.prepare<{ isbn: string }, { author_id: string }>(`SELECT author_id FROM book WHERE isbn = :isbn`)
+//     //     this.get.one({isbn: 'sdfb'})
+//     // }
+
+//     // select_book_and_author = query`SELECT ${[Book.params.author_id, Author.params.name]} FROM book INNER JOIN ...`
+// }
+
+// const book = new Book()
+
+// book.select_one.one({ author_id: 1 }) // missing isbn param
+// book.select_one.one({ author_id: 1, isbn: -1 }) // isbn should be a string
+// book.select_one.one({ author_id: 1, isbn: 'abcd-1234' }) // isbn should be a string
+export { Model } from './model.ts'
+export { schema } from './schema.ts'
+export { query } from './query.ts'
+export * as z from 'https://deno.land/x/zod@v3.18.0/mod.ts'
--- test/usage.test.ts
+++ test/usage.test.ts
@@ -1,5 +1,40 @@
-import { torm } from '../src/mod.ts'
+import { Database } from '../../sqlite-native/src/mod.ts'
+import { Model, schema, query, z } from '../src/mod.ts'
+
+class Author extends Model {
+
+  static schema = schema('author', {
+    id:         z.number(),
+    first_name: z.string(),
+    last_name:  z.string(),
+  })
+}
+
+class Book extends Model {
+
+  static schema = schema('book', {
+    id:         z.number(),
+    isbn:       z.string(),
+    author_id:  z.number(),
+    title:      z.string(),
+  })
+
+  // TODO we need to be able to rename fields. Likely this will be: `${Author.schema.id('author_id')}`
+  // TODO columns need to include table name, like author.id
+  get = query`SELECT ${[Book.schema.id, Book.schema.isbn]} FROM book`
+}
+
 
 Deno.test('usage', async () => {
-  console.log('hi')
+  const driver = new Database('test.db')
+  await driver.connect()
+
+  const book = new Book()
+  const id: number = book.get.params.id
+  const isbn: string = book.get.params.isbn
+  // const db = torm(driver, { FooBar })
+
+  // db.models.FooBar.schema
+
+  driver.close()
 })
